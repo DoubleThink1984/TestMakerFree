723761cf9557c9a0a2e8782ffa8a3ca9f29ca99e
Devin Cook <d.a.cook1@outlook.com>
2018-03-11 16:06:08 -0500

    add additional controllers and viewModels. Implement Rest methods.

--- TestMakerFreeWebApp/Controllers/QuizController.cs
+++ TestMakerFreeWebApp/Controllers/QuizController.cs
@@ -1,24 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TestMakerFreeWebApp.ViewModels;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TestMakerFreeWebApp.Controllers
 {
     [Route("api/[controller]")]
     public class QuizController : Controller
     {
-        // GET api/quiz/latest
-        [HttpGet("Latest/{num}")]
+        #region RESTful conventions methods
+        /// <summary>
+        /// GET: api/quiz/{id}
+        /// Retrieves the Quiz with the given {id}
+        /// </summary>
+        /// <param name="id">The ID of an existing Quiz</param>
+        /// <returns>the Quiz with the given {id}</returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // create a sample quiz to match the given request
+            var v = new QuizViewModel()
+            {
+                Id = id,
+                Title = String.Format("Sample quiz with id {0}", id),
+                Description = "Not a real quiz: it's just a sample!",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            };
+
+            // output the result in JSON format
+            return new JsonResult(
+                v,
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+        /// <summary>
+        /// Adds a new Quiz to the Database
+        /// </summary>
+        /// <param name="model">The QuizViewModel containing the data to insert</param>
+        [HttpPut]
+        public IActionResult Put(QuizViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Edit the Quiz with the given {id}
+        /// </summary>
+        /// <param name="model">The QuizViewModel containing the data to update</param>
+        [HttpPost]
+        public IActionResult Post(QuizViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes the Quiz with the given {id} from the Database
+        /// </summary>
+        /// <param name="id">The ID of an existing Test</param>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Attribute-based routing methods
+        /// <summary>
+        /// GET: api/quiz/latest
+        /// Retrieves the {num} latest Quizzes
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>the {num} latest Quizzes</returns>
+        [HttpGet("Latest/{num:int?}")]
         public IActionResult Latest(int num = 10)
         {
             var sampleQuizzes = new List<QuizViewModel>
             {
+
                 // add a first sample quiz
                 new QuizViewModel()
                 {
@@ -35,8 +99,8 @@ namespace TestMakerFreeWebApp.Controllers
             {
                 sampleQuizzes.Add(new QuizViewModel()
                 {
-                    Id = 1,
-                    Title = $"Sample Quiz {i}",
+                    Id = i,
+                    Title = String.Format("Sample Quiz {0}", i),
                     Description = "This is a sample quiz",
                     CreatedDate = DateTime.Now,
                     LastModifiedDate = DateTime.Now
@@ -53,18 +117,19 @@ namespace TestMakerFreeWebApp.Controllers
         }
 
         /// <summary>
-        /// Get: api/quiz/ByTitle
+        /// GET: api/quiz/ByTitle
         /// Retrieves the {num} Quizzes sorted by Title (A to Z)
         /// </summary>
-        /// <param name="num">Quizzes sorted by Title</param>
-        /// <returns></returns>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} Quizzes sorted by Title</returns>
         [HttpGet("ByTitle/{num:int?}")]
         public IActionResult ByTitle(int num = 10)
         {
-            var sampleQuizes = ((JsonResult)Latest(num)).Value as List<QuizViewModel>;
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value
+                as List<QuizViewModel>;
 
             return new JsonResult(
-                sampleQuizes.OrderBy(t => t.Title),
+                sampleQuizzes.OrderBy(t => t.Title),
                 new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
@@ -72,21 +137,24 @@ namespace TestMakerFreeWebApp.Controllers
         }
 
         /// <summary>
-        /// Get: api/quiz/mostViewed
-        /// Retrieves the {num} random Quizes
+        /// GET: api/quiz/mostViewed
+        /// Retrieves the {num} random Quizzes
         /// </summary>
-        /// <param name="num"></param>
+        /// <param name="num">the number of quizzes to retrieve</param>
         /// <returns>{num} random Quizzes</returns>
         [HttpGet("Random/{num:int?}")]
         public IActionResult Random(int num = 10)
         {
-            var sampleQuizes = ((JsonResult)Latest(num)).Value as List<QuizViewModel>;
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value
+                as List<QuizViewModel>;
 
-            return new JsonResult(sampleQuizes.OrderBy(t => Guid.NewGuid()),
+            return new JsonResult(
+                sampleQuizzes.OrderBy(t => Guid.NewGuid()),
                 new JsonSerializerSettings()
                 {
                     Formatting = Formatting.Indented
                 });
         }
+        #endregion
     }
 }
