86563acb5bc8150075ec522d217531b522cd9f49
DoubleThink1984 <d.a.cook1@outlook.com>
2018-08-03 17:03:18 -0500

    in progress

--- TestMakerFreeWebApp/ClientApp/app/components/question/question-edit.component.ts
+++ TestMakerFreeWebApp/ClientApp/app/components/question/question-edit.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: "question-edit",
@@ -11,6 +12,7 @@ import { HttpClient } from "@angular/common/http";
 export class QuestionEditComponent {
     title: string;
     question: Question;
+    form: FormGroup;
 
     // this will be TRUE when editing an existing question, 
     //   FALSE when creating a new one.
@@ -19,10 +21,12 @@ export class QuestionEditComponent {
     constructor(private activatedRoute: ActivatedRoute,
         private router: Router,
         private http: HttpClient,
+        private fb: FormBuilder,
         @Inject('BASE_URL') private baseUrl: string) {
 
         // create an empty object from the Quiz interface
         this.question = <Question>{};
+        this.createForm();
 
         var id = +this.activatedRoute.snapshot.params["id"];
 
@@ -37,6 +41,8 @@ export class QuestionEditComponent {
                 this.question = result;
                 this.title = "Edit - " + this.question.Text;
             }, error => console.error(error));
+
+            this.updateForm();
         }
         else {
             this.question.QuizId = id;
@@ -44,21 +50,27 @@ export class QuestionEditComponent {
         }
     }
 
-    onSubmit(question: Question) {
+    onSubmit() {
         var url = this.baseUrl + "api/question";
 
-        if (this.editMode) {
+        var tempQuestion = <Question>{};        
+        tempQuestion.QuizId = this.question.QuizId;
+        tempQuestion.Text = this.form.value.Text;
+
+        if (this.editMode) {            
+            tempQuestion.Id = this.question.Id;
+
             this.http
-                .put<Question>(url, question)
+                .put<Question>(url, tempQuestion)
                 .subscribe(res => {
                     var v = res;
                     console.log("Question " + v.Id + " has been updated.");
                     this.router.navigate(["quiz/edit", v.QuizId]);
-                }, error => console.log(error));
+                }, error => console.log(error));            
         }
         else {
             this.http
-                .post<Question>(url, question)
+                .post<Question>(url, tempQuestion)
                 .subscribe(res => {
                     var v = res;
                     console.log("Question " + v.Id + " has been created.");
@@ -67,7 +79,38 @@ export class QuestionEditComponent {
         }
     }
 
+    createForm() {
+        this.form = this.fb.group({
+            Text: ['', Validators.required]
+        });
+    }
+
+    updateForm() {
+        this.form.setValue({
+            Text: this.question.Text || ''
+        });
+    }
+
     onBack() {
         this.router.navigate(["quiz/edit", this.question.QuizId]);
+    }
+
+    getFormControl(name: string) {
+        return this.form.get(name);
+    }
+
+    isValid(name: string) {
+        var e = this.getFormControl(name);
+        return e && e.valid;
+    }
+
+    isChanged(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched);
+    }
+
+    hasError(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched) && !e.valid;
     }
 }
